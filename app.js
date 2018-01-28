05857f4874a9937c89d17810a746cd637f8f3818
quinlayen <quinlayen@gmail.com>
2018-01-27 15:25:53 -1000

    scoring and player selection operating

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-/*
-GAME RULES:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
 
 // function to determine which player it is and change players
 
@@ -22,10 +13,70 @@ GAME RULES:
 let score = [];
 let roundScore = 0;
 let activePlayer = 0;
+let dice;
+let globalScore;
+let totalDice = 0;
+let currentScore;
 
-//let dice = Math.floor(Math.random()*6)+1
+//this function starts a new game
+let newGame = function(){
+    
+}
 
-dice = 1;
-   if (dice === 1){
-       document.getElementById('dice').src=("dice-1.png")
-   }
+//this function is used to change the active player
+let changeActivePlayer = function(){
+    if (activePlayer === 0){
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+};
+
+
+
+// this function is used to change the globalScoring variable
+let globalScoring = function(){
+    if (activePlayer === 0){
+        document.querySelector('#score-0').textContent=score[0]=(totalDice);
+        changeActivePlayer();
+    } else {
+        document.querySelector('#score-1').textContent=score[1]=(totalDice);
+        changeActivePlayer();
+    }
+};
+
+// this function controls the roll mechanism of the game
+let roll = function(){
+    dice = Math.floor(Math.random()*6)+1;
+    if (dice === 1){
+        document.getElementById('dice').src=('dice-1.png');
+    } else if (dice === 2){
+        document.getElementById('dice').src=('dice-2.png');
+    } else if (dice === 3) {
+        document.getElementById('dice').src=('dice-3.png');
+    } else if (dice === 4) {
+        document.getElementById('dice').src=('dice-4.png');
+    } else if (dice === 5) {
+        document.getElementById('dice').src=('dice-5.png');
+    } else {
+        document.getElementById('dice').src=('dice-6.png');
+    }
+
+    if (dice !== 1){
+        totalDice = dice + totalDice;
+        currentScoring();
+    } else {
+        changeActivePlayer();
+    }
+};
+
+let currentScoring = function(){
+    if (activePlayer === 0){
+        currentScore = document.getElementById('current-0').textContent = totalDice;
+    } else {
+        currentScore = document.getElementById('current-1').textContent = totalDice;
+    }
+}
+
+document.querySelector('.btn-roll').addEventListener('click', roll);
+document.querySelector('.btn-hold').addEventListener('click', globalScoring);
